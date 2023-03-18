7ea7a99b627eb07d9339d5a22e5dbe69d2a0ff3f
Ravinder Sharma <106022307+ChiefRavinder@users.noreply.github.com>
2023-03-18 18:58:25 +0530

    added btn

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -8,42 +8,50 @@ import xbox from '../public/icons/xbox.svg'
 
 export default function HeroSection() {
   return <section>
-  <div className="relative w-screen h-screen">
-    <Image
-      className='cursor-pointer object-cover h-full w-full'
-      src={im}
-      alt='/'
-    />
-  <div className="absolute flex flex-col  text-2.25 text-yellow-500 top-56 left-56">
-  <div style={{ marginRight: '20px' }}>
-    <Image
-      className='cursor-pointer object-cover h-full w-full'
-      src={logo}
-      alt='/'
-     />
-  </div>
-  <div style={{ marginTop: '10px'}} className="flex flex-row pd-5 justify-center">
-    <Image
-      className='cursor-pointer object-cover w-6 filter-invert mr-2'
-      src={pc}
-      alt='/'/>
-    <Image
-      className='cursor-pointer object-cover w-6 mr-2'
-      src={pc}
-      alt='/'/>
-    <Image
-      className='cursor-pointer object-cover w-6 mr-2'
-      src={pc}
-      alt='/'/>
-  </div>
-  <div className="text-2.25 flex justify-center" style={{ textShadow: '0.1875rem 0.1875rem 0.3125rem rgba(0,0,0,.3)' }}>AVAILABLE
-</div>     
-<div className=" text-2.25 flex justify-center" style={{ textShadow: '0.1875rem 0.1875rem 0.3125rem rgba(0,0,0,.3)' }}>
-03.07.2023
-</div>
+    <div className="relative w-screen h-screen">
+      <Image
+        className='cursor-pointer object-cover h-full w-full'
+        src={im}
+        alt='/'
+      />
+      <div className="absolute flex flex-col  text-2.25 text-yellow-500 top-56 left-56">
+        <div style={{ marginRight: '20px' }}>
+          <Image
+            className='cursor-pointer object-cover h-full w-full'
+            src={logo}
+            alt='/'
+          />
+        </div>
+        <div style={{ marginTop: '10px' }} className="flex flex-row pd-5 justify-center">
+          <Image
+            className='cursor-pointer object-cover w-6 filter-invert mr-2'
+            src={pc}
+            alt='/' />
+          <Image
+            className='cursor-pointer object-cover w-6 mr-2'
+            src={pc}
+            alt='/' />
+          <Image
+            className='cursor-pointer object-cover w-6 mr-2'
+            src={pc}
+            alt='/' />
+        </div>
+        <div className="text-2.25 flex justify-center" style={{ textShadow: '0.1875rem 0.1875rem 0.3125rem rgba(0,0,0,.3)' }}>AVAILABLE
+        </div>
+        <div className=" text-2.25 flex justify-center" style={{ textShadow: '0.1875rem 0.1875rem 0.3125rem rgba(0,0,0,.3)' }}>
+          03.07.2023
+        </div>
+        <div>
 
-</div>
+        <div className="h-14 border-2 border-white text-white flex justify-center items-center hover:bg-white hover:text-black duration-100">
+        <div>Watch Trailer</div>
+        </div>
+        <div className="h-14 border-2 border-white text-white flex justify-center items-center hover:bg-white hover:text-black duration-100">
+        <div>Buy Now</div>
+        </div>
+        </div>
+      </div>
 
-  </div>
-</section>
+    </div>
+  </section>
 }
