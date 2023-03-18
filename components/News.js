61509e5b62ca23dfeb781d7c375e5ddc32fc6f2e
Ravinder Sharma <106022307+ChiefRavinder@users.noreply.github.com>
2023-03-18 19:45:19 +0530

    added second ui

--- components/News.js
+++ components/News.js
@@ -2,22 +2,22 @@
 
 export default function News() {
     return <section>
-        <div className="relative w-screen h-screen bg-blue-400">
+        <div className="relative w-screen  bg-blue-400 min-h-100">
 
             
-                <div className="text-2xl text-yellow-200 flex flex-column justify-center text-yellow">
+                <div className="text-2xl text-yellow-200 flex flex-column justify-center text-yellow ">
                     WELCOME TO
                 </div>
                 <div className="text-4xl text-yellow-200 flex flex-column justify-center text-yellow">
                     THE FUTURE
                 </div>
-                <div className="text-4xl text-yellow-200 flex flex-column justify-center text-yellow">
+                <div className="text-4xl text-yellow-200 flex flex-column justify-center text-yellow ">
                 TRY NOT TO BREAK IT
                 </div>
-                <div className="text-4xl text-yellow-200 flex flex-column justify-center text-yellow max-w-16">
+                <div className="text-4xl text-yellow-200 flex flex-column justify-center text-yellow ml-20 mr-20">
                 Lost in transit while on a colonist ship bound for the furthest edge of the galaxy, you awake decades later than you expected only to find yourself in the midst of a deep conspiracy threatening to destroy the Halcyon colony.
                 </div>
-                <div className="text-4xl text-yellow-200 flex flex-column justify-center text-yellow max-w-64">
+                <div className="text-4xl text-yellow-200 flex flex-column justify-center text-yellow ml-20 mr-20">
                 As you explore the furthest reaches of space and encounter a host of factions all vying for power, who you decide to become will determine the fate of everyone in Halcyon. In the corporate equation for the colony, you are the unplanned variable.
                 </div>
 
