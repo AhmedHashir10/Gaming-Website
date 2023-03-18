4e028f3485d61c698532f3cabdbb56139b97869c
Ravinder Sharma <106022307+ChiefRavinder@users.noreply.github.com>
2023-03-18 19:02:05 +0530

    btn style improved

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -41,12 +41,11 @@ export default function HeroSection() {
         <div className=" text-2.25 flex justify-center" style={{ textShadow: '0.1875rem 0.1875rem 0.3125rem rgba(0,0,0,.3)' }}>
           03.07.2023
         </div>
-        <div>
-
-        <div className="h-14 border-2 border-white text-white flex justify-center items-center hover:bg-white hover:text-black duration-100">
+        <div className="flex justify-center items-center gap-2">
+        <div className="h-14 border-2 px-8 border-white text-white flex justify-center items-center hover:bg-white hover:text-black duration-100">
         <div>Watch Trailer</div>
         </div>
-        <div className="h-14 border-2 border-white text-white flex justify-center items-center hover:bg-white hover:text-black duration-100">
+        <div className="h-14 w-fit px-4 text-white flex justify-center items-center bg-red-600 hover:bg-blue-400 duration-200">
         <div>Buy Now</div>
         </div>
         </div>
